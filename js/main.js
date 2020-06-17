56f0756b25c6518ccecee1be8eea3d7e1dc22727
Serena <serenafayelombardo@gmail.com>
2020-06-17 11:12:44 +0200

    Added menu mobile functionality, fixed active states menu on mobile

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
+const menuButton = document.querySelector('.header__mobile-menu__button');
+const menuButtonImage = menuButton.querySelector('img');
+const menuBlock = document.querySelector('.header__menu');
 
+menuButton.addEventListener('click', () => {
+  if (menuBlock.classList.contains('header__menu__hidden')) {
+    menuBlock.classList.remove('header__menu__hidden');
+    menuButtonImage.setAttribute('src', 'images/icon-close.svg');
+    menuButton.setAttribute('aria-expanded', 'true');
+  } else {
+    menuBlock.classList.add('header__menu__hidden');
+    menuButtonImage.setAttribute('src', 'images/icon-hamburger.svg');
+    menuButton.setAttribute('aria-expanded', 'false');
+  }
+});
+
+document.addEventListener('keyup', (e) => {
+  if (!menuBlock.classList.contains('header__menu__hidden') && e.key === 'Escape') {
+    menuBlock.classList.add('header__menu__hidden');
+    menuButtonImage.setAttribute('src', 'images/icon-hamburger.svg');
+    menuButton.setAttribute('aria-expanded', 'false');
+  }
+});
